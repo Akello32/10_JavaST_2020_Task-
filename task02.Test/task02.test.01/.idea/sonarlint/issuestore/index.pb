1a9f8aca176c5f87353edb41455214d8246ffc54
Matvej Mukhlia <matmux23@gmail.com>
2020-09-23 17:44:20 +0300

    Создал класс Hello

---
+++ task02.Test/task02.test.01/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Y
+)src/main/java/by/matmux/Runner/Hello.java,3\8\38f7f7bc3ce366ce5bea51d65396eadb01f4ddfd
---
+++ task02.Test/task02.test.01/src/main/java/by/matmux/Runner/Hello.java
@@ -0,0 +1,7 @@
+package by.matmux.Runner;
+
+    public class Hello {
+        public static void main(String[] args) {
+            System.out.println("Hello " + args[0]);
+        }
+    }
