082c9b2444e61014b76444f0de91f7e2869f4815
Matvej Mukhlia <matmux23@gmail.com>
2020-09-23 23:32:30 +0300

    Задание 2 в Task 2

---
+++ task02.Test/task02.Test.02/src/main/java/by/matmux/Runner/Runner.java
@@ -0,0 +1,9 @@
+package by.matmux.Runner;
+
+public class Runner {
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.println(args[i]);
+        }
+    }
+}
--- task02.Test/task02.test.01/src/main/java/by/matmux/Runner/Hello.java
+++ task02.Test/task02.test.01/src/main/java/by/matmux/Runner/Hello.java
@@ -1,7 +1,7 @@
 package by.matmux.Runner;
 
-    public class Hello {
-        public static void main(String[] args) {
-            System.out.println("Hello " + args[0]);
-        }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
     }
+}
